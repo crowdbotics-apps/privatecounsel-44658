1c681758d6319f874c2d0110988e7a0f04dec2ad
Crowdbotics <team@crowdbotics.com>
2023-11-18 04:47:56 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "privatecounsel-44658",
+  url: "https://privatecounsel-44658.botics.co",
+  api: "https://privatecounsel-44658.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
